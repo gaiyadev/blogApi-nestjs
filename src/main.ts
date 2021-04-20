564cc8d6dacb7b6dcf7776163e160e9c7f36eb0e
gaiyadev <gaiyaobed94@gmail.com>
2021-04-20 05:17:11 +0100

    Config

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
   const version = 'v1';
   app.setGlobalPrefix(`api/${version}`);
-  await app.listen(3000);
+  const PORT = 3000;
+  await app.listen(PORT);
+  logger.log(`Application listening on port ${PORT}`);
 }
 bootstrap();
